afc6d11d9574f1ade6dec8cd609ed2f404fd139d
Andres <andrsbtrg@gmail.com>
2024-09-22 10:37:16 +0200

    Day 1

--- src/entry.c
+++ src/entry.c
@@ -1,4 +1,5 @@
-#include "../raylib/include/raylib.h"
+#include "raylib.h"
+#include "raymath.h"
 #include <stdlib.h>
 
 typedef enum EntityArchetype {
@@ -20,17 +21,16 @@ typedef struct World {
 } World;
 
 World *world;
+void init_entities(World *world);
+void update_player(Entity *player);
+void setup_window(void);
 
 #define PLAYER_HEIGHT 80
 #define PLAYER_WIDTH 60
 
-int main(void) {
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    InitWindow(screenWidth, screenHeight, "Game");
 
-    // Setup world
-    world = (World *)malloc(sizeof(World));
+int main(void) {
+    setup_window();
 
     Image player = LoadImage("../assets/player.png");
     if (player.data == NULL) {
@@ -39,23 +39,24 @@ int main(void) {
     Texture2D texture = LoadTextureFromImage(player);
     UnloadImage(player);
 
+    // Setup world
+    world = (World *)malloc(sizeof(World));
+
+    init_entities(world);
+
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
-        // Get the DPI scaling factor
-        Vector2 scale = GetWindowScaleDPI();
+        // Handle Keys
+        Entity * player = &world->entities[0];
+        update_player(player);
 
-        // Calculate the centered position considering the DPI scaling
-        Rectangle player_rec = {
-            (screenWidth - 40) / 2 * scale.x, // Center horizontally
-            (screenHeight - 40) / 2 * scale.y, // Center vertically
-            40, 40
-        };
-
-        Vector2 mouse = GetMousePosition();
+        // Drawing
         BeginDrawing();
+
         ClearBackground(DARKGRAY);
-        DrawRectangleRec(player_rec, RED);
-        DrawText(TextFormat("Default Mouse: [%i , %i]", (int)mouse.x, (int)mouse.y), 
+        DrawTextureV(texture, player->position, WHITE);
+
+        DrawText(TextFormat("Player pos: [%i , %i]", (int)player->position.x, (int)player->position.y),
                  400, 25, 20, RED);
         EndDrawing();
     }
@@ -64,4 +65,51 @@ int main(void) {
     CloseWindow();
 
     return 0;
+}
+
+void init_entities(World * world) {
+    // initial player position
+    Vector2 player_pos = { 0, 0 };
+
+    Entity player = { 0 };
+    player.arch = ARCH_PLAYER;
+    player.position = player_pos;
+    player.is_valid = true;
+
+    world->entities[0] = player;
+
+    return;
+}
+
+void update_player(Entity *player) {
+    // move player
+    Vector2 input_axis = Vector2 { 0.0, 0.0 };
+    // left
+    if (IsKeyDown(KEY_A)) {
+        input_axis.x = -1;
+    }
+    if (IsKeyDown(KEY_D)) {
+        input_axis.x = 1;
+    }
+    if (IsKeyDown(KEY_S)) {
+        input_axis.y = 1;
+    }
+    if (IsKeyDown(KEY_W)) {
+        input_axis.y = -1;
+    }
+    input_axis = Vector2Normalize(input_axis);
+
+    Vector2 initial_pos = (Vector2) { player->position.x, player->position.y};
+    Vector2 movement = Vector2Scale(input_axis, 10);
+    player->position = Vector2Add(initial_pos, movement);
+    return;
+}
+void setup_window() {
+    int screenWidth = 800;
+    int screenHeight = 600;
+    InitWindow(screenWidth, screenHeight, "Game");
+
+    Vector2 scale = GetWindowScaleDPI();
+    SetWindowSize(screenWidth / scale.x, screenHeight / scale.y);
+    return;
 }
